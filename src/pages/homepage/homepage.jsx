10cdb1e5e11471ce56fc1b3515ab2f714a0c469a
rdm100 <robertode_marco@hotmail.com>
2021-02-22 17:03:20 +0000

    Converted homepage, header, custom-button and collection-item into styled components

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './homepage.scss';
+import { HomepageContainer } from './homepage-styles';
 import Directory from '../../components/directory/directory';
 
 const Homepage = () => {
 	return (
-		<div className='homepage'>
+		<HomepageContainer>
 			<Directory />
-		</div>
+		</HomepageContainer>
 	);
 };
 
